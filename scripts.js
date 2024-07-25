b54f4358d50adf324b6591a3d1287b6eab77a8de
Mark Reed <mreed@cd-mark.local>
2024-07-25 16:44:37 -0400

    complete image

--- scripts.js
+++ scripts.js
@@ -1,27 +1,57 @@
-
 document.querySelector('#add_new_todo_btn').addEventListener('click', addNewTodo)
 document.querySelector('.board').addEventListener('dragover', e => e.preventDefault())
 document.querySelectorAll('.tasks').forEach(column => {
     this.addEventListener('drop', handleDrop)
 })
-let todos = []
+let todoTaskArr = JSON.parse(localStorage.getItem('kanbanTodoTaskArr')) || []
+let inProgressTaskArr = JSON.parse(localStorage.getItem('kanbanInProgressTaskArr')) || []
+let completeTaskArr = JSON.parse(localStorage.getItem('kanbanCompleteTaskArr')) || []
 function saveTodoTasks(){
-    todos = []
+    todoTaskArr = []
     const todoTasks = document.querySelectorAll('#todo_column .task')
     todoTasks.forEach(todo => {
-        console.log(todo.childNodes)
         const taskData = {
             uid: todo.id.split("_")[1],
             title: todo.childNodes[5].innerText,
             description: todo.childNodes[9].innerText,
             column: "todo_column"
         }
-        todos.push(taskData)
+        todoTaskArr.push(taskData)
     })
-    localStorage.setItem("kanbanTodos", JSON.stringify(todos))
+    localStorage.setItem("kanbanTodoTaskArr", JSON.stringify(todoTaskArr))
 }
-function saveDateToLocalStorage(){
+function saveInProgressTasks(){
+    inProgressTaskArr = []
+    const inProgressTasks = document.querySelectorAll('#in_progress_column .task')
+    inProgressTasks.forEach(task => {
+        const taskData = {
+            uid: task.id.split("_")[1],
+            title: task.childNodes[5].innerText,
+            description: task.childNodes[9].innerText,
+            column: "in_progress_column"
+        }
+        inProgressTaskArr.push(taskData)
+    })
+    localStorage.setItem("kanbanInProgressTaskArr", JSON.stringify(inProgressTaskArr))
+}
+function saveCompleteTasks(){
+    completeTaskArr = []
+    const completeTasks = document.querySelectorAll('#complete_column .task')
+    completeTasks.forEach(task => {
+        const taskData = {
+            uid: task.id.split("_")[1],
+            title: task.childNodes[5].innerText,
+            description: task.childNodes[9].innerText,
+            column: "complete_column"
+        }
+        completeTaskArr.push(taskData)
+    })
+    localStorage.setItem("kanbanCompleteTaskArr", JSON.stringify(completeTaskArr))
+}
+function saveAllTasks(){
     saveTodoTasks()
+    saveInProgressTasks()
+    saveCompleteTasks()
 }
 function handleTitleBlur(e){
     if(e.target.value){
@@ -29,7 +59,7 @@ function handleTitleBlur(e){
         document.getElementById(`h2_${uid}`).innerText = e.target.value
         e.target.classList.add('hidden')
         document.getElementById(`h2_${uid}`).classList.remove('hidden')
-        saveDateToLocalStorage()
+        saveTodoTasks()
     }
 }
 function handleDescriptionBlur(e){
@@ -38,7 +68,7 @@ function handleDescriptionBlur(e){
         document.getElementById(`p_${uid}`).innerText = e.target.value
         e.target.classList.add('hidden')
         document.getElementById(`p_${uid}`).classList.remove('hidden')
-        saveDateToLocalStorage()
+        saveTodoTasks()
     }
 }
 function handleTitleClick(e){
@@ -49,7 +79,6 @@ function handleTitleClick(e){
     titleInput.classList.remove('hidden')
     e.target.classList.add('hidden')
 }
-
 function handleDescriptionClick(e){
     const uid = e.target.id.split('_')[1]
     const text = e.target.innerText
@@ -61,14 +90,25 @@ function handleDescriptionClick(e){
 function handleDeleteClick(e){
     const uid = e.target.id.split('_')[1]
     console.log('delete ' + uid)
+    todoTaskArr = todoTaskArr.filter(todo => {
+        return todo.uid !== uid
+    })
+    inProgressTaskArr= inProgressTaskArr.filter(todo => {
+        return todo.uid !== uid
+    })
+    completeTaskArr= completeTaskArr.filter(todo => {
+        return todo.uid !== uid
+    })
+    document.querySelector(`#task_${uid}`).remove()
+    saveAllTasks()
 }
 let draggedCard
 function handleDragStart(e){
     draggedCard = e.target
 }
 function handleDrop(e){
-    console.log("test123")
     e.target.append(draggedCard)
+    saveAllTasks()
 }
 function addTaskListeners(id){
     document.querySelector(`#title_${id}`).addEventListener('blur', handleTitleBlur)
@@ -92,20 +132,29 @@ function addNewTodo(){
     document.querySelector('.todo.tasks').insertAdjacentHTML('beforeend', newTask)
     addTaskListeners(uid)
 }
-function displayStoredTasks(){
-    const storedTasks = JSON.parse(localStorage.getItem('kanbanTodos'))
-    storedTasks.forEach(task => {
-        const taskHtml = `
-            <div class="task" id="task_${task.uid}" draggable="true">
-                <button id="delete_${task.uid}" class="delete-btn">X</button>
-                <input id="title_${task.uid}" type="text" placeholder="title" class="hidden"/>
-                <h2 id="h2_${task.uid}">${task.title}</h2>
-                <textarea id="description_${task.uid}" placeholder="description" class="hidden"/></textarea>
-                <p id="p_${task.uid}">${task.description}</p>
-            </div>
-        `
-        document.querySelector('.todo.tasks').insertAdjacentHTML('beforeend', taskHtml)
-        addTaskListeners(task.uid)
-    })
+function displayTasks(tasks, selector){
+    if(tasks){
+        tasks.forEach(task => {
+            const taskHtml = `
+                <div class="task" id="task_${task.uid}" draggable="true">
+                    <button id="delete_${task.uid}" class="delete-btn">X</button>
+                    <input id="title_${task.uid}" type="text" placeholder="title" class="hidden"/>
+                    <h2 id="h2_${task.uid}">${task.title}</h2>
+                    <textarea id="description_${task.uid}" placeholder="description" class="hidden"/></textarea>
+                    <p id="p_${task.uid}">${task.description}</p>
+                </div>
+            `
+            document.querySelector(selector).insertAdjacentHTML('beforeend', taskHtml)
+            addTaskListeners(task.uid)
+        })
+    }
+    
 }
-displayStoredTasks()
+function init(){
+console.log('init.')
+   displayTasks(JSON.parse(localStorage.getItem('kanbanTodoTaskArr')), '#todo_tasks') 
+   displayTasks(JSON.parse(localStorage.getItem('kanbanInProgressTaskArr')), '#in_progress_tasks') 
+   displayTasks(JSON.parse(localStorage.getItem('kanbanCompleteTaskArr')), '#complete_tasks') 
+}
+
+init()
